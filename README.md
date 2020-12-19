38c853dc491f57875ddeb24f7b20a28ce3a672d0
Elton Leite <borelanjo@gmail.com>
2020-12-18 23:55:16 -0300

    :tada: corrige pasta do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+## Ensine seus filhos a programar
+
+Estudo com o livro Ensine seus filhos a programar
---
+++ cap1/MadLib.py
@@ -0,0 +1,6 @@
+substantivo = input('Diga um substantivo: ')
+adjetivo = input('Diga um adjetivo: ')
+verbo = input('Diga um verbo: ')
+
+print('Your Madlib:')
+print('O(a)', substantivo, adjetivo, verbo, 'em cima do cachorro preguiçoso')
