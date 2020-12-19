ac5abc9d95570d3039203634af894fc04b5cea4f
Elton Leite <borelanjo@gmail.com>
2020-12-18 23:59:05 -0300

    Update MadLib.py

--- MadLib.py
+++
@@ -1,6 +0,0 @@
-substantivo = input('Diga um substantivo: ')
-adjetivo = input('Diga um adjetivo: ')
-verbo = input('Diga um verbo: ')
-
-print('Your Madlib:')
-print('O(a)', substantivo, adjetivo, verbo, 'em cima do cachorro preguiçoso')
